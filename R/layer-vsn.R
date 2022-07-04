7f48d531a52e250a50bb75d129ec1ba3445ba868
Krzysztof Joachimiak <joachimiak.krzysztof@gmail.com>
2022-07-04 23:49:10 +0200

    VSN tweaks

--- R/layer-vsn.R
+++ R/layer-vsn.R
@@ -1,6 +1,13 @@
-#'  Variable Selection Network block
+#' Variable Selection Network block
+#'
+#' It receives four-dimensional vector as an input in the case of dynamic data
+#' (batch_size, timesteps, n_features, feature_dim)
 #'
 #' @param dropout_rate Droput rate
+#' @inheritParams layer-grn.R
+#'
+#' @returns
+#' A t
 #'
 #' @include layer-grn.R
 #'
@@ -13,35 +20,54 @@ layer_vsn <- keras::new_layer_class(
     hidden_dim,
     state_size,
     dropout_rate = NULL,
+    return_weights = FALSE,
     ...){
 
-    super()$`__init__`(name = name, ...)
+    super()$`__init__`(...)
 
     self$hidden_dim   <- hidden_dim
     self$state_size   <- state_size
     self$dropout_rate <- dropout_rate
+    self$use_context  <- use_context
 
   },
 
   build = function(input_shape){
 
-    num_features <- as.integer(input_shape[2])
-    state_size   <- as.integer(input_shape[3])
+    # GRN do generowania wag
+    # na wejściu (batch_size, num_inputs * state_size)
+    # Wejście do GRN od wag ma inny kształt, niż wejście do indywidualnych GRNów
+    # Softmax daje wartoœści per feature wejściowy:
+    # np. 0.56 dla ceny, 0.1 dla temepratury itp.
+    # Ale żeby to policzyć, warstwa dense musi widzieć "wszystko"
+
+
+    num_features <- as.integer(rev(input_shape)[2])
+    state_size   <- as.integer(rev(input_shape)[1])
+
+    self$reshape <- layer_lambda(
+      f = function(x) k_reshape(x, c(
+        -1, as.numeric(input_shape[2]),
+        num_features * state_size
+      ))
+    )
 
     self$weights_grn <- layer_grn(
       input_shape  = num_features * state_size,
       hidden_units = self$hidden_dim,
       output_size  = num_features,
-      dropout_rate = self$dropout_rate
+      dropout_rate = self$dropout_rate,
+      use_context  = self$use_context
     )
 
-    # Tutaj jest indeksowanie z Pythona, trzeba uważać
+    # Be careful: Python indexing
     self$softmax <- layer_activation_softmax(axis = -1)
 
     # Dodatkowo, każda zmienna dostaje własną warstwę GRN
     self$input_grns <- as.list(vector(length = num_features))
 
     self$num_features <- num_features
+
 
     # Do tych GRN-ów nie dodajemy kontekstu
     for (i in 1:num_features) {
@@ -60,12 +86,12 @@ layer_vsn <- keras::new_layer_class(
   call = function(inputs, context = NULL){
 
     # Wejście (batch_size, n_features, state_size)
-
     # Liczymy wagi dla każdego feature'u wejściowego
     # Pamiętajmy, że każda zmienna została zrzutowana krok wcześniej na wiele ficzerów
+    # browser()
 
-    inputs_to_weights <- layer_flatten()(inputs)
-    variable_weights  <- self$weights_grn(inputs_to_weights)
+    inputs_to_weights <- self$reshape(inputs)
+    variable_weights  <- self$weights_grn(inputs_to_weights, context)
     variable_weights  <- self$softmax(variable_weights)
     variable_weights  <- layer_lambda(f = function(x) k_expand_dims(x, -1))(variable_weights)
 
@@ -74,23 +100,26 @@ layer_vsn <- keras::new_layer_class(
     # Aplikujemy indywidualne wartwy GRN
     for (i in 1:self$num_features) {
       # layer_lambda do utrzymania wymiarów
-      inp <- layer_lambda(f = function(x) k_expand_dims(x, 2))(inputs[,i,])
+      inp <- layer_lambda(f = function(x) k_expand_dims(x, 3))(inputs[,,i,])
       processed <- self[[as.character(i)]](inp)
       processed_inputs <- append(processed_inputs, processed)
     }
 
     # Składamy wszystko z powrotem
-    processed_inputs <- layer_concatenate(processed_inputs, axis = 1)
+    processed_inputs <- layer_concatenate(processed_inputs, axis = 2)
     outputs <- processed_inputs * variable_weights
 
     # Likwidujemy drugi wymiar (liczba zmiennych kategorycznych)
     # Zostaje wymiar oznaczający state size
-    outputs <- layer_lambda(f = function(x) k_sum(x, axis = 2))(outputs)
+    outputs <- layer_lambda(f = function(x) k_sum(x, axis = 3))(outputs)
 
     # Dimensions:
     # outputs: [num_samples x state_size]
-    list(outputs, variable_weights)
-  }
 
+    if (self$return_weights)
+      return(list(outputs, variable_weights))
+    else
+      return(outputs)
+  }
 
 )
