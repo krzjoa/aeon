b8edb4a2f81b6dc9333141ba975ba7c6269d8d18
Krzysztof Joachimiak <joachimiak.krzysztof@gmail.com>
2022-09-11 19:40:42 +0200

    alloc arrays

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -11,21 +11,25 @@ Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
 // get_arrays
-List get_arrays(int n_samples, int lookback, int n_features);
-RcppExport SEXP _aion_get_arrays(SEXP n_samplesSEXP, SEXP lookbackSEXP, SEXP n_featuresSEXP) {
+List get_arrays(DataFrame data, DataFrame data_index, DataFrame slices, int lookback, int horizon, List past_var, List fut_var);
+RcppExport SEXP _aion_get_arrays(SEXP dataSEXP, SEXP data_indexSEXP, SEXP slicesSEXP, SEXP lookbackSEXP, SEXP horizonSEXP, SEXP past_varSEXP, SEXP fut_varSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< int >::type n_samples(n_samplesSEXP);
+    Rcpp::traits::input_parameter< DataFrame >::type data(dataSEXP);
+    Rcpp::traits::input_parameter< DataFrame >::type data_index(data_indexSEXP);
+    Rcpp::traits::input_parameter< DataFrame >::type slices(slicesSEXP);
     Rcpp::traits::input_parameter< int >::type lookback(lookbackSEXP);
-    Rcpp::traits::input_parameter< int >::type n_features(n_featuresSEXP);
-    rcpp_result_gen = Rcpp::wrap(get_arrays(n_samples, lookback, n_features));
+    Rcpp::traits::input_parameter< int >::type horizon(horizonSEXP);
+    Rcpp::traits::input_parameter< List >::type past_var(past_varSEXP);
+    Rcpp::traits::input_parameter< List >::type fut_var(fut_varSEXP);
+    rcpp_result_gen = Rcpp::wrap(get_arrays(data, data_index, slices, lookback, horizon, past_var, fut_var));
     return rcpp_result_gen;
 END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_aion_get_arrays", (DL_FUNC) &_aion_get_arrays, 3},
+    {"_aion_get_arrays", (DL_FUNC) &_aion_get_arrays, 7},
     {NULL, NULL, 0}
 };
 
--- src/code.cpp
+++ src/code.cpp
@@ -6,15 +6,25 @@ List get_arrays(DataFrame data, DataFrame data_index, DataFrame slices,
                 int lookback, int horizon, List past_var, List fut_var){
   List list = List::create();
 
+  // alocate an array of names
+
   // Allocate past arrays
   for (int i = 0; i < past_var.length(); i++){
     // list
+    list.push_back(1);
   }
 
   // Allocate future arrays
   for (int i = 0; i < fut_var.length(); i++){
     // list
+    // allocate an array
+    // add names(?)
+    list.push_back(2);
   }
+
+  // assign list of names to the list
+
+  // https://rcppcore.github.io/RcppParallel/
 
   return list;
 }
