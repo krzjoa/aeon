5b2e1ac613892ff7a1686cf5c063ecc501e19d82
Krzysztof Joachimiak <joachimiak.krzysztof@gmail.com>
2022-09-15 22:29:52 +0200

    make_arrays works

--- R/make-arrays.R
+++ R/make-arrays.R
@@ -1,11 +1,11 @@
+#' @keywords internal
 resolve_variables <- function(period_set, type_set){
   if (is.null(period_set))
     return(type_set)
   base::intersect(period_set, type_set)
 }
 
-
-#' Prepare arrays (tensors), which can be used a
+#' Prepare input/taget arrays for time series models
 #'
 #' @inheritParams as_3d_array
 #' @param lookback The length of the context from the past
@@ -18,12 +18,23 @@ resolve_variables <- function(period_set, type_set){
 #' @param categorical Categorical variables
 #' @param static Static variables
 #'
+#' @returns
+#' A list of arrays. The maximal possible content embraces eight arrays:
+#' - **y_past**
+#' - **X_past_cat**
+#' - **X_past_num**
+#' - **y_fut**
+#' - **X_fut_cat**
+#' - **X_fut_num**
+#' - **X_static_cat**
+#' - **X_static_num**
+#'
 #' @examples
 #' library(m5)
-#' library(recipes)
+#' library(recipes, warn.conflicts=FALSE)
 #' library(zeallot)
 #' library(dplyr, warn.conflicts=FALSE)
-#' library(data.table)
+#' library(data.table, warn.conflicts=FALSE)
 #'
 #' train <- tiny_m5[date < '2016-01-01']
 #' test  <- tiny_m5[date >= '2016-01-01']
@@ -43,19 +54,22 @@ resolve_variables <- function(period_set, type_set){
 #'
 #' TARGET      <- 'value'
 #' STATIC      <- c('item_id_idx', 'store_id_idx')
-#' CATEGORICAL <- c('event_name_1', 'event_type_1')
-#' NUMERIC     <- c('sell_price')
+#' CATEGORICAL <- c('event_name_1', 'event_type_1', STATIC)
+#' NUMERIC     <- c('sell_price', 'sell_price')
+#' KEY         <- c('item_id', 'store_id')
+#' INDEX       <- 'date'
 #' LOOKBACK    <- 28
-#' HORIZON     <- 28
+#' HORIZON     <- 14
 #' STRIDE      <- LOOKBACK
 #'
 #' setDT(train)
+#' setDT(test)
 #'
-#' output <-
+#' train_arrays <-
 #'    make_arrays(
 #'        data = train,
-#'        key = c('item_id', 'store_id'),
-#'        index = 'date',
+#'        key = KEY,
+#'        index = INDEX,
 #'        lookback = LOOKBACK,
 #'        horizon = HORIZON,
 #'        stride = STRIDE,
@@ -65,11 +79,30 @@ resolve_variables <- function(period_set, type_set){
 #'        numeric=NUMERIC
 #'    )
 #'
+#' print(names(train_arrays))
+#' print(dim(train_arrays$X_past_num))
+#'
+#' test_arrays <-
+#'    make_arrays(
+#'        data = test,
+#'        key = KEY,
+#'        index = INDEX,
+#'        lookback = LOOKBACK,
+#'        horizon = HORIZON,
+#'        stride = STRIDE,
+#'        target=TARGET,
+#'        static=STATIC,
+#'        categorical=CATEGORICAL,
+#'        numeric=NUMERIC
+#'    )
+#'
+#' print(names(test_arrays))
+#' print(dim(test_arrays$X_past_num))
 #' @export
 make_arrays <- function(data, key, index, lookback,
                         horizon, stride=1, shuffle=TRUE, sample_frac = 1.,
                         target, numeric=NULL, categorical=NULL, static=NULL,
-                        past=NULL, future=NULL, keep_all_ids=TRUE, ...){
+                        past=NULL, future=NULL, ...){
 
   # Start of each time series we can identify with unique key
   total_window_length <- lookback + horizon
@@ -94,8 +127,6 @@ make_arrays <- function(data, key, index, lookback,
       ts_starts[,.SD[sample(.N,as.integer(floor(.N * sample_frac)))]
                 ,by = eval(key)]
 
-  # Consider using Rcpp here to speed it up
-  # One loop instead of separate ones
   # Static
   if (!is.null(static))
     static_categorical <- resolve_variables(static, categorical)
@@ -115,6 +146,10 @@ make_arrays <- function(data, key, index, lookback,
       X_static_num <-
         as.matrix(static_features[, ..static_numeric])
 
+  static_arrays <-
+    list(X_static_cat=X_static_cat,
+         X_static_num=X_static_num)
+
   setnames(ts_starts, 'window_start', index)
 
   # Sort & create indices
@@ -124,43 +159,42 @@ make_arrays <- function(data, key, index, lookback,
   data[, row_idx := 1:.N]
   ts_starts[data, row_idx := row_idx, on=(eval(key_index))]
 
+  # Resolve variables
+  past_cat <- resolve_variables(past, categorical)
+  past_num <- resolve_variables(past, numeric)
+  fut_cat  <- resolve_variables(future, categorical)
+  fut_num  <- resolve_variables(future, numeric)
+
+  # Convert to numeric - required by the Rcpp function, which uses NumericVector
+  all_dynamic_vars <- c(categorical, numeric, target)
+
+  for (v in all_dynamic_vars)
+    data.table::set(data, j = v, value = as.numeric(data[[v]]))
+
   # Dynamic variables
   past_var <-
     list(
-      y_past     = target#,
-      #X_past_cat = categorical
-      #X_past_num = numeric
-    )
-
-  past_var_types <-
-    c(
-      y_past     = 'numeric',
-      X_past_cat = 'categorical',
-      X_past_num = 'numeric'
+      y_past     = target,
+      X_past_num = past_num,
+      X_past_cat = past_cat
     )
 
   fut_var <-
     list(
-      y_fut      = target#,
-      #X_fut_cat  = categorical
-      #X_fut_num  = numeric
+      y_fut      = target,
+      X_fut_num  = fut_num,
+      X_fut_cat  = fut_cat
     )
 
-  fut_var_types <- c('numeric')
-
-  #browser()
-
-  dynamic <-
-    aion:::get_arrays(
+  dynamic_arrays <-
+    get_arrays(
       data           = data,
       ts_starts      = ts_starts,
       lookback       = lookback,
       horizon        = horizon,
       past_var       = past_var,
-      past_var_types = past_var_types,
-      fut_var        = fut_var,
-      fut_var_types  = fut_var_types
+      fut_var        = fut_var
     )
 
-  dynamic
+  c(dynamic_arrays, static_arrays)
 }
